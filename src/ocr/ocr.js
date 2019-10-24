b30d8b24ad05c7e091831a235f056f090d590668
Mr. Ayoung <saing.sab@gmail.com>
2019-10-24 11:43:20 +0700

    get full content

--- src/ocr/ocr.js
+++ src/ocr/ocr.js
@@ -1,53 +1,62 @@
-const Tesseract = require('tesseract.js')
-var request = require('request')
-var fs = require('fs')
+const Tesseract = require("tesseract.js");
+var request = require("request");
+const uuid = require("uuid/v4");
+var fs = require("fs");
 
 let url;
 
-var genName = new Date().getTime()
-var filename = `./temp/${genName}`
+var genName = uuid();
+var filename = `./temp/${genName}`;
 
-const readPromise = function(){
-    return new Promise(function(resolve, reject){
-        var writeFile = fs.createWriteStream(filename)
-        request(url).pipe(writeFile).on('close', function() {
-          Tesseract.recognize(filename)
-          .progress(function  (p) { console.log('progress', p)  })
+const readPromise = function() {
+  return new Promise(function(resolve, reject) {
+    var writeFile = fs.createWriteStream(filename);
+    request(url)
+      .pipe(writeFile)
+      .on("close", function() {
+        Tesseract.recognize(filename)
+          .progress(function(p) {
+            console.log("progress", p);
+          })
           .catch(err => console.error(err))
-          .then(function (result) {
+          .then(function(result) {
             console.log(result);
             var res = result.text.split("\n");
-            resolve(res)
-          })
-        })
-    })
-}
+            resolve(res);
+          });
+      });
+  });
+};
 
 exports.ocr = async ctx => {
-    var _amount = ctx.request.body.amount;
-    var _trxdate = ctx.request.body.trxdate;
-    url = ctx.request.body.imguri;
-     
-      var _xtAmount;
-      const data = await readPromise(url);
-      var lengthOfData = data.length;
-      for(i = 0; i < lengthOfData; i++) {
-        for(y = 0; y < lengthOfData; y++){
-          var rawData = data[y];
-          if(true == data[y].startsWith(_amount)){
-            _xtAmount = rawData;
-          }
-        }
-        if(true == data[i].startsWith(_trxdate)){
-          var obj = {
-                      idx: i,
-                      trxdate: data[i],
-                      amount: _xtAmount
-                    }
-          console.log(JSON.stringify(obj));
-          ctx.status = 200;
-          ctx.body = { data: obj };
-          return 0;
-        }
-      }
-}
+  // var _amount = ctx.request.body.amount;
+  // var _trxdate = ctx.request.body.trxdate;
+  url = ctx.request.body.imguri;
+
+  // var _xtAmount;
+  const data = await readPromise(url);
+  // var lengthOfData = data.length;
+  // for (i = 0; i < lengthOfData; i++) {
+  //   for (y = 0; y < lengthOfData; y++) {
+  //     var rawData = data[y];
+  //     if (true == data[y].startsWith(_amount)) {
+  //       _xtAmount = rawData;
+  //     }
+  //   }
+  // if (true == data[i].startsWith(_trxdate)) {
+  //   var obj = {
+  //     idx: i,
+  //     trxdate: data[i],
+  //     amount: _xtAmount
+  //   };
+  //   console.log(JSON.stringify(obj));
+  //   ctx.status = 200;
+  //   ctx.body = { data: obj };
+  //   return 0;
+  // }
+  // }
+  console.log(data);
+  ctx.status = 200;
+  ctx.body = { data: { uuid: genName, content: data } };
+  return 0;
+};
--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const _ = require("koa-route");
 const cors = require("@koa/cors");
 const bodyParser = require("koa-bodyparser");
 const json = require("koa-json");
-const koaBody = require("koa-body");
 const ocr = require("../src/ocr/ocr");
 
 const App = new Koa();
